a8fa7883a67db24c511fc272bcac7a5d8c4ef044
Dora <dora.kasik@fer.hr>
2023-11-03 00:20:01 +0100

    Add routing to button on the landing page
    Added routing functionality to a 'PRIJAVI SE' button on the landing page. It now redirects the user to login page when clicked.

--- src/components/LandingMain.jsx
+++ src/components/LandingMain.jsx
@@ -1,12 +1,15 @@
 import React from "react";
+import ButtonLink from './ButtonLink'
 
+const handleClick = (e) => {
+    e.preventDefault();
+    // Do sth
+}
 function LandingMain() {
     return(
         <div className="w-screen h-screen bg-landing-bg bg-cover bg-center pt-28">
             <h1 className="whitespace-pre-line text-8xl font-bold text-sky-700 ml-10">Mi brinemo{'\n'} o Vašem{'\n'} oporavku.</h1>
-            <div className="flex justify-start pl-11">
-                <button className="bg-sky-600 p-5 text-white font-bold rounded-[5px] mt-12 mb-10"> PRIJAVI SE </button>
-            </div>
+            <ButtonLink />
         </div>
     );
 }
