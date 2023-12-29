418d8b914eaeeab3ec8d1400c4cd2e73eaa06a59
begJosip <josipbegic57@gmail.com>
2023-12-29 14:48:34 +0100

    Add SUPERADMIN room managment

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,30 +1,44 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import PageLayout from "../components/PageLayout";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import UserDashMain from "../components/UserDashMain";
 import EmployeeDashMain from "../components/EmployeeDashMain";
 import SuperAdminDashboard from "../components/SuperAdminDashboard";
+import {useNavigate} from "react-router-dom";
 
 function Dashboard() {
 
-    /* Za isprobati ucitavanje potrebnog dashboarda */
-    let user = "SUPERADMIN";
+    let navigate = useNavigate();
+
+    useEffect(() => {
+        let roles = localStorage.getItem('roles');
+        if (roles.includes('SUPERADMIN')) {
+            setUserRole('SUPERADMIN');
+            return;
+        }
+        if (roles.includes('PATIENT')) {
+            setUserRole('PATIENT')
+            return;
+        }
+        if (roles.includes('EMPLOYEE')) {
+            setUserRole('EMPLOYEE')
+            return;
+        }
+        navigate('/pageNotFound');
+    }, [navigate]);
+
+    const [userRole, setUserRole] = useState('');
 
     return (
         <>
             <PageLayout>
                 <Header/>
-                {user === "PATIENT" ? (
-                    <UserDashMain/>
-                ) : user === "EMPLOYEE" ? (
-                    <EmployeeDashMain/>
-                ) : (
-                    <SuperAdminDashboard/>
-                )}
+                {userRole === "PATIENT" && <UserDashMain/>}
+                {userRole === "EMPLOYEE" && <EmployeeDashMain/>}
+                {userRole === 'SUPERADMIN' && <SuperAdminDashboard/>}
                 <Footer/>
-                {/* Kad se Footer koristi s UserMain-om pojavi se scroll x */}
-                {/*<Footer/>*/}
+                { /* TODO Kad se Footer koristi s UserMain-om pojavi se scroll x */}
             </PageLayout>
         </>
     );
