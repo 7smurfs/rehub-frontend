3bc35a14b74eb4ca0726ce59f90fa5764d4a874a
Dora <dora.kasik@fer.hr>
2024-01-04 01:50:14 +0100

    Add basic layout

--- src/components/EmployeeDashMain.jsx
+++ src/components/EmployeeDashMain.jsx
@@ -2,12 +2,39 @@ import React from "react";
 
 function EmployeeDashMain() {
     return(
-        <div className="h-full border border-amber-900 grid grid-cols-2 grid-rows-3">
-            <div className={"row-span-2 col-span-1 border border-amber-900"}></div>
-            <div className={"row-span-1 col-span-1 border border-amber-900"}></div>
-            <div className={"row-span-1 col-span-1 border border-amber-900"}></div>
-            <div className={"row-span-1 col-span-1 border border-amber-900"}></div>
-            <div className={"row-span-1 col-span-1 border border-amber-900"}></div>
+        <div className="h-full grid grid-cols-3 grid-rows-2 gap-2 p-2">
+            <div className={"h-full row-span-1 col-span-2 bg-sky-100 rounded-[5px]"}>
+                <div className={'h-10 rounded-tl-[5px] rounded-tr-[5px] flex items-center justify-center'}>
+                    <span className={'text-sky-600 font-bold text-2xl'}>Kalendar</span>
+                </div>
+                <div className={'h-4/5 w-full'}>
+
+                </div>
+            </div>
+            <div className={"h-full row-span-2 col-span-1 bg-sky-100 rounded-[5px]"}>
+                <div className={'h-10 flex justify-center items-center'}>
+                    <span className={'text-sky-600 font-bold text-2xl'}>Pregled prijava</span>
+                </div>
+                <div className={'h-5/6 overflow-y-scroll bg-sky-50 mx-2'}>
+
+                </div>
+            </div>
+            <div className={"row-span-1 col-span-1 bg-sky-100 rounded-[5px]"}>
+                <div className={'h-10 flex justify-center items-center'}>
+                    <span className={'text-sky-600 font-bold text-2xl'}>Sobe/Ordinacije</span>
+                </div>
+                <div className={'h-4/5 bg-sky-50 mx-2 overflow-y-scroll'}>
+
+                </div>
+            </div>
+            <div className={"row-span-1 col-span-1 bg-sky-100 rounded-[5px]"}>
+                <div className={'h-10 flex justify-center items-center'}>
+                    <span className={'text-sky-600 font-bold text-2xl'}>Oprema</span>
+                </div>
+                <div className={'h-4/5 bg-sky-50 mx-2 overflow-y-scroll'}>
+
+                </div>
+            </div>
         </div>
     );
 }
