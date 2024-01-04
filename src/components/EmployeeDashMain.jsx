73dc1411e5d415f13301fd71c0301b899a253130
Dora <dora.kasik@fer.hr>
2024-01-04 19:40:00 +0100

    Add loading appointment requests, rooms and equipment

--- src/components/EmployeeDashMain.jsx
+++ src/components/EmployeeDashMain.jsx
@@ -1,14 +1,51 @@
-import React from "react";
-import AppointmentRequest from "./AppointmentRequest";
+import React, {useState} from "react";
 import Arrow from "../assets/right-arrow.svg";
 import {Link} from "react-router-dom";
+import api from "../http/api";
+import {toast} from "react-toastify";
 
 function EmployeeDashMain() {
 
-    let ct = 0;
+    const [roomList, setRoomList] = useState([]);
+    const [equipmentList, setEquipmentList] = useState([]);
+    const [apptsList, setApptsList] = useState([]);
+
+    const showData = async () => {
+        try {
+            const [roomRes, equipmentRes, apptsRes] = await Promise.all([
+                api.get('/employee/room', {
+                    headers: {
+                        'Authorization': 'Bearer ' + localStorage.getItem('token')
+                    }
+                }),
+
+                api.get('/employee/equipment', {
+                    headers: {
+                        'Authorization': 'Bearer ' + localStorage.getItem('token')
+                    }
+                }),
+
+                api.get('/employee/therapies', {
+                    headers: {
+                        'Authorization': 'Bearer ' + localStorage.getItem('token')
+                    }
+                })
+            ])
+
+            setRoomList(roomRes.data);
+            setEquipmentList(equipmentRes.data);
+            setApptsList(apptsRes.data);
+            console.log(apptsRes.data)
+
+        } catch (err) {
+            toast.error("Provjerite internetsku vezu.");
+        }
+    };
+
+
 
     return(
-        <div className="h-[75%] grid grid-cols-4 grid-rows-3 gap-2 p-2">
+        <div className="h-[75%] grid grid-cols-4 grid-rows-3 gap-2 p-2" onLoad={showData}>
             <div className={"h-full row-span-3 col-span-2 bg-sky-100 rounded-[5px]"}>
                 <div className={'h-10 rounded-tl-[5px] rounded-tr-[5px] flex items-center justify-center'}>
                     <span className={'text-sky-600 font-bold text-2xl'}>Kalendar</span>
@@ -21,24 +58,24 @@ function EmployeeDashMain() {
                 <div className={'h-10 flex justify-center items-center'}>
                     <span className={'text-sky-600 font-bold text-2xl'}>Pregled prijava</span>
                 </div>
-                {ct === 0 ? (
+                {apptsList.length === 0 ? (
                     <div className={'bg-sky-50 h-5/6 mx-2 flex items-center justify-center'}>
                         <span className={'text-gray-500 text-lg'}>Trenutno nema aktivnih prijava.</span>
                     </div>
                 ) : (
                     <div className={'h-5/6 overflow-y-scroll bg-sky-50 mx-2 p-3 flex flex-col gap-3'}>
-                        {/*<AppointmentRequest/>
-                        <AppointmentRequest/>
-                        <AppointmentRequest/>
-                        <AppointmentRequest/>
-                        <AppointmentRequest/>
-                        <AppointmentRequest/>
-                        <AppointmentRequest/>
-                        <AppointmentRequest/>
-                        <AppointmentRequest/>
-                        <AppointmentRequest/>
-                        <AppointmentRequest/>
-                        <AppointmentRequest/>*/}
+                        {apptsList.map((appt, key) => (
+                            <div key={key} className={'bg-sky-200 w-full flex justify-between py-3 pl-3'}>
+                                <div className={'h-full flex items-center'}>
+                                    <span className={'text-sky-800 font-bold'}>{appt.patientResponse.firstName} {appt.patientResponse.lastName}</span>
+                                </div>
+                                <div className={'w-20 flex items-center justify-center'}>
+                                    <Link to={'/appointment'}>
+                                        <img src={Arrow} alt="Arrow" className={'h-10'}/>
+                                    </Link>
+                                </div>
+                            </div>
+                        ))}
                     </div>
                 )}
 
@@ -47,13 +84,24 @@ function EmployeeDashMain() {
                 <div className={'h-10 flex justify-center items-center'}>
                     <span className={'text-sky-600 font-bold text-2xl'}>Sobe/Ordinacije</span>
                 </div>
-                {ct === 0 ? (
+                {roomList.length === 0 ? (
                     <div className={'h-4/6 bg-sky-50 mx-2 flex justify-center items-center'}>
                         <span className={'text-gray-500 text-lg'}>Nema dostupnih soba.</span>
                     </div>
                 ) : (
                     <div className={'h-4/5 bg-sky-50 mx-2 overflow-y-scroll'}>
-
+                        {roomList.map((room, key) => (
+                            <div
+                                key={key}
+                                className={room.status === 'OPERABLE' ?
+                                    'bg-white p-4 text-sky-900 flex flex-row justify-between text-sm m-4' :
+                                    'bg-gray-300 p-4 text-gray-500 flex flex-row justify-between rounded-lg text-2xl m-4'}>
+                                <span className={'font-bold text-lg'}>{room.label}</span>
+                                <div className={'flex flex-row justify-center items-center'}>
+                                    <span className={'font-bold'}>Kapacitet: {room.capacity}</span>
+                                </div>
+                            </div>
+                        ))}
                     </div>
                 )}
 
@@ -62,13 +110,24 @@ function EmployeeDashMain() {
                 <div className={'h-10 flex justify-center items-center'}>
                     <span className={'text-sky-600 font-bold text-2xl'}>Oprema</span>
                 </div>
-                {ct === 0 ? (
+                {equipmentList.length === 0 ? (
                     <div className={'h-4/6 bg-sky-50 mx-2 flex justify-center items-center'}>
                         <span className={'text-gray-500 text-lg'}>Nema dostupne opreme.</span>
                     </div>
                 ) : (
                     <div className={'h-4/5 bg-sky-50 mx-2 overflow-y-scroll'}>
-
+                        {equipmentList.map((equipment, key) => (
+                            <div
+                                key={key}
+                                className={equipment.status === 'OPERABLE' ?
+                                    'bg-white p-4 text-sky-900 flex flex-row justify-between text-sm m-4' :
+                                    'bg-gray-300 p-4 text-gray-500 flex flex-row justify-between rounded-lg text-2xl m-4'}>
+                                <span className={'font-bold text-lg'}>{equipment.name}</span>
+                                <div className={'flex flex-row justify-center items-center'}>
+                                    <span className={'font-bold'}>Soba: </span>
+                                </div>
+                            </div>
+                        ))}
                     </div>
                 )}
 
