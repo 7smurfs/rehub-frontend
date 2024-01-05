023267a867ffa5287e9e10b791134b451f305dd1
begJosip <josipbegic57@gmail.com>
2024-01-05 16:42:36 +0100

    Fix registration form

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
-import { useNavigate } from "react-router-dom";
+import React, {useState} from 'react';
+import {useNavigate} from "react-router-dom";
 import api from "../http/api";
-import { toast, ToastContainer } from "react-toastify";
+import {toast, ToastContainer} from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 import ReCAPTCHA from "react-google-recaptcha";
 import show from "../assets/show-pass.svg";
@@ -31,8 +31,8 @@ function RegisterForm() {
     const [showRepeatPass, setShowRepeatPass] = useState(false);
 
     const handleChange = (e) => {
-        const { name, value } = e.target;
-        setRegisterData({ ...registerData, [name]: value });
+        const {name, value} = e.target;
+        setRegisterData({...registerData, [name]: value});
     };
 
     const validateRegisterData = () => {
@@ -62,7 +62,7 @@ function RegisterForm() {
         if (/^\d+$/.test(registerData.pin)) {
             if (registerData.pin.length !== 11)
                 toast.info("OIB nije ispravne duljine")
-                setStep(tmp => 2)
+            setStep(tmp => 2)
         } else {
             toast.info("OIB mora sadržavati 11 brojki.")
             setStep(tmp => 2)
@@ -71,7 +71,7 @@ function RegisterForm() {
         if (/^\d+$/.test(registerData.phin)) {
             if (registerData.phin.length !== 9)
                 toast.info("MBO nije ispravne duljine")
-                setStep(tmp => 2)
+            setStep(tmp => 2)
         } else {
             toast.info("MBO mora sadržavati 9 brojki.")
             setStep(tmp => 2)
@@ -124,6 +124,11 @@ function RegisterForm() {
             setStep(tmp => tmp + 1);
         }
     }
+    const decrementStep = () => {
+        if (step > 0) {
+            setStep(tmp => tmp - 1);
+        }
+    }
 
     return (
         <>
@@ -132,56 +137,63 @@ function RegisterForm() {
                     className="w-90 h-120 bg-white [box-shadow:-2px_15px_30px_rgba(23,_37,_84,_0.2)] rounded-[10px] mt-8 flex flex-col">
                     <div
                         className="w-full h-24 bg-sky-600 opacity-50 rounded-tl-[10px] rounded-tr-[10px] flex justify-center items-end pb-4">
-                        <img src={userIcon} alt="User icon" className="w-[32px] h-[32px] mr-1" />
+                        <img src={userIcon} alt="User icon" className="w-[32px] h-[32px] mr-1"/>
                         <span className="text-white font-bold text-3xl ml-1">Registracija</span>
                     </div>
                     <div className="h-2 w-full bg-sky-600 opacity-50 ">
-                        <div className="h-2 bg-sky-900 transition-all duration-500" style={{ width: `${(step / 4) * 100}%` }}></div>
+                        <div className="h-2 bg-sky-900 transition-all duration-500"
+                             style={{width: `${(step / 4) * 100}%`}}></div>
                     </div>
                     <form className="flex flex-col gap-0 w-full justify-items-center p-10">
                         {step === 0 && (<section>
                             <div className="">
-                                <label className="font-bold text-sky-600 text-lg mt-[15px] self-start block">Ime:</label>
+                                <label
+                                    className="font-bold text-sky-600 text-lg mt-[15px] self-start block">Ime:</label>
                                 <input type="text" name="firstName" id="firstName" value={registerData.firstName}
-                                    onChange={handleChange}
-                                    className="w-[400px] h-[40px] bg-sky-200 opacity-50 mb-[2px] rounded-[5px] p-2" />
+                                       onChange={handleChange}
+                                       className="w-[400px] h-[40px] bg-sky-200 opacity-50 mb-[2px] rounded-[5px] p-2"/>
                             </div>
 
                             <div className="">
-                                <label className="font-bold text-sky-600 text-lg mt-[15px] self-start block">Prezime:</label>
+                                <label
+                                    className="font-bold text-sky-600 text-lg mt-[15px] self-start block">Prezime:</label>
                                 <input type="text" name="lastName" id="lastName" value={registerData.lastName}
-                                    onChange={handleChange}
-                                    className="w-[400px] h-[40px] bg-sky-200 opacity-50 mb-[2px] rounded-[5px] p-2" />
+                                       onChange={handleChange}
+                                       className="w-[400px] h-[40px] bg-sky-200 opacity-50 mb-[2px] rounded-[5px] p-2"/>
                             </div>
                         </section>)}
                         {step === 1 && (<section>
                             <div className="">
-                                <label className="font-bold text-sky-600 text-lg mt-[15px] self-start block">E-mail:</label>
+                                <label
+                                    className="font-bold text-sky-600 text-lg mt-[15px] self-start block">E-mail:</label>
                                 <input type="email" name="username" id="username" value={registerData.username}
-                                    onChange={handleChange}
-                                    className="w-[400px] h-[40px] bg-sky-200 opacity-50 mb-[2px] rounded-[5px] p-2" />
+                                       onChange={handleChange}
+                                       className="w-[400px] h-[40px] bg-sky-200 opacity-50 mb-[2px] rounded-[5px] p-2"/>
                             </div>
 
                             <div className="">
                                 <label className="font-bold text-sky-600 text-lg mt-[15px] self-start block">Broj
                                     telefona:</label>
                                 <input type="text" name="phoneNumber" id="phoneNumber" value={registerData.phoneNumber}
-                                    onChange={handleChange}
-                                    className="w-[400px] h-[40px] bg-sky-200 opacity-50 mb-[2px] rounded-[5px] p-2" />
+                                       onChange={handleChange}
+                                       className="w-[400px] h-[40px] bg-sky-200 opacity-50 mb-[2px] rounded-[5px] p-2"/>
                             </div>
                         </section>)}
 
                         {step === 2 && (<section>
                             <div className="">
-                                <label className="font-bold text-sky-600 text-lg mt-[15px] self-start block">OIB:</label>
+                                <label
+                                    className="font-bold text-sky-600 text-lg mt-[15px] self-start block">OIB:</label>
                                 <input type="text" name="pin" id="pin" value={registerData.pin} onChange={handleChange}
-                                    className="w-[400px] h-[40px] bg-sky-200 opacity-50 mb-[2px] rounded-[5px] p-2" />
+                                       className="w-[400px] h-[40px] bg-sky-200 opacity-50 mb-[2px] rounded-[5px] p-2"/>
                             </div>
 
                             <div className="col-span-1">
-                                <label className="font-bold text-sky-600 text-lg mt-[15px] self-start block">MBO:</label>
-                                <input type="text" name="phin" id="phin" value={registerData.phin} onChange={handleChange}
-                                    className="w-[400px] h-[40px] bg-sky-200 opacity-50 mb-[2px] rounded-[5px] p-2" />
+                                <label
+                                    className="font-bold text-sky-600 text-lg mt-[15px] self-start block">MBO:</label>
+                                <input type="text" name="phin" id="phin" value={registerData.phin}
+                                       onChange={handleChange}
+                                       className="w-[400px] h-[40px] bg-sky-200 opacity-50 mb-[2px] rounded-[5px] p-2"/>
                             </div>
 
                         </section>)}
@@ -190,15 +202,15 @@ function RegisterForm() {
                                 <label className="font-bold text-sky-600 text-lg mt-[15px] self-start block">Datum
                                     rođenja:</label>
                                 <input type="date" name="dateOfBirth" id="dateOfBirth" value={registerData.dateOfBirth}
-                                    onChange={handleChange}
-                                    className="w-[400px] h-[40px] bg-sky-200 opacity-50 mb-[2px] rounded-[5px] p-2" />
+                                       onChange={handleChange}
+                                       className="w-[400px] h-[40px] bg-sky-200 opacity-50 mb-[2px] rounded-[5px] p-2"/>
                             </div>
 
                             <div className="">
                                 <label htmlFor={'gender'}
-                                    className="font-bold text-sky-600 text-lg mt-[15px] self-start block">Spol:</label>
+                                       className="font-bold text-sky-600 text-lg mt-[15px] self-start block">Spol:</label>
                                 <select name="gender" id="gender" value={registerData.gender} onChange={handleChange}
-                                    className="w-[400px] h-[40px] bg-sky-200 opacity-50 mb-[2px] rounded-[5px] p-2">
+                                        className="w-[400px] h-[40px] bg-sky-200 opacity-50 mb-[2px] rounded-[5px] p-2">
                                     <option value={""}>Odaberi spol</option>
                                     <option value={"MALE"}>Muško</option>
                                     <option value={"FEMALE"}>Žensko</option>
@@ -209,26 +221,28 @@ function RegisterForm() {
                         </section>)}
                         {step === 4 && (<section>
                             <div className="">
-                                <label className="font-bold text-sky-600 text-lg mt-[15px] self-start block">Lozinka:</label>
+                                <label
+                                    className="font-bold text-sky-600 text-lg mt-[15px] self-start block">Lozinka:</label>
                                 <div className="relative">
-                                    <input type={showPass ? "text" : "password"} name="password" id="password" value={registerData.password}
-                                        onChange={handleChange}
-                                        className="w-[400px] h-[40px] bg-sky-200 opacity-50 mb-[2px] rounded-[5px] p-2" />
+                                    <input type={showPass ? "text" : "password"} name="password" id="password"
+                                           value={registerData.password}
+                                           onChange={handleChange}
+                                           className="w-[400px] h-[40px] bg-sky-200 opacity-50 mb-[2px] rounded-[5px] p-2"/>
 
                                     <img src={showPass ? hide : show} onClick={togglePass}
-                                        className="w-6 absolute top-[22%] left-[91%] cursor-pointer" alt={'passEye'} />
+                                         className="w-6 absolute top-[22%] left-[91%] cursor-pointer" alt={'passEye'}/>
                                 </div>
-
                             </div>
-
                             <div className="">
-                                <label className="font-bold text-sky-600 text-lg mt-[15px] self-start block">Ponovljena lozinka:</label>
+                                <label className="font-bold text-sky-600 text-lg mt-[15px] self-start block">Ponovljena
+                                    lozinka:</label>
                                 <div className="relative">
-                                    <input type={showRepeatPass ? "text" : "password"} name="confirmPassword" id="confirmPassword"
-                                        value={registerData.confirmPassword} onChange={handleChange}
-                                        className="w-[400px] h-[40px] bg-sky-200 opacity-50 mb-[2px] rounded-[5px] p-2" />
+                                    <input type={showRepeatPass ? "text" : "password"} name="confirmPassword"
+                                           id="confirmPassword"
+                                           value={registerData.confirmPassword} onChange={handleChange}
+                                           className="w-[400px] h-[40px] bg-sky-200 opacity-50 mb-[2px] rounded-[5px] p-2"/>
                                     <img src={showRepeatPass ? hide : show} onClick={toggleRepeatPass}
-                                        className="w-6 absolute top-[22%] left-[91%] cursor-pointer" alt={'eyePass'} />
+                                         className="w-6 absolute top-[22%] left-[91%] cursor-pointer" alt={'eyePass'}/>
                                 </div>
 
                             </div>
@@ -241,15 +255,42 @@ function RegisterForm() {
                             </div>
                         </section>)}
 
-                        <div className="flex justify-start space-x-4">
-                            <button
-                                type='button'
-                                className="bg-sky-600 text-white font-semibold px-6 py-3 my-10 rounded-[5px]"
-                                onClick={incrementStep}>Dalje
-                            </button>
+                        <div className="flex justify-between space-x-4">
+                            {step === 0 &&
+                                <button
+                                    disabled={(/^\s*$/.test(registerData.firstName) || /^\s*$/.test(registerData.lastName))}
+                                    type='button'
+                                    className={"bg-sky-600 text-white font-semibold px-6 py-3 my-10 rounded-[5px]"}
+                                    onClick={incrementStep}>Dalje
+                                </button>
+                            }
+                            {step === 1 &&
+                                <button
+                                    disabled={(/^\s*$/.test(registerData.username) || /^\s*$/.test(registerData.phoneNumber))}
+                                    type='button'
+                                    className="bg-sky-600 text-white font-semibold px-6 py-3 my-10 rounded-[5px]"
+                                    onClick={incrementStep}>Dalje
+                                </button>
+                            }
+                            {step === 2 &&
+                                <button
+                                    disabled={(/^\s*$/.test(registerData.pin) || /^\s*$/.test(registerData.phin))}
+                                    type='button'
+                                    className="bg-sky-600 text-white font-semibold px-6 py-3 my-10 rounded-[5px]"
+                                    onClick={incrementStep}>Dalje
+                                </button>
+                            }
+                            {step === 3 &&
+                                <button
+                                    disabled={(/^\s*$/.test(registerData.dateOfBirth))}
+                                    type='button'
+                                    className="bg-sky-600 text-white font-semibold px-6 py-3 my-10 rounded-[5px]"
+                                    onClick={incrementStep}>Dalje
+                                </button>
+                            }
                             {step > 3 && (
                                 <button
-                                    disabled={!captchaValid && true}
+                                    disabled={!captchaValid}
                                     className={captchaValid ?
                                         "bg-sky-600 text-white font-semibold px-6 py-3 my-10 rounded-[5px]"
                                         :
@@ -257,13 +298,19 @@ function RegisterForm() {
                                     }
                                     onClick={handleSubmit}
                                 >
-                                    Register
+                                    Registriraj se
                                 </button>
                             )}
+                            <button
+                                disabled={step < 1}
+                                type='button'
+                                className="bg-sky-600 text-white font-semibold px-6 py-3 my-10 rounded-[5px]"
+                                onClick={decrementStep}>Natrag
+                            </button>
                         </div>
                     </form>
-                </div >
-            </div >
+                </div>
+            </div>
             <ToastContainer
                 position="bottom-right"
                 autoClose={2000}
