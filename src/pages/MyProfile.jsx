c1a6ef6307b6d72af017277fdeb175d9fc1f1f94
begJosip <josipbegic57@gmail.com>
2024-01-06 16:59:53 +0100

    FIX: fix axios request catch and error toast

--- src/pages/MyProfile.jsx
+++ src/pages/MyProfile.jsx
@@ -24,11 +24,11 @@ const MyProfile = () => {
 
     useEffect(() => {
         const fetchData = async () => {
-                await api.get('/stats/user', {
-                    headers: {
-                        'Authorization': 'Bearer ' + localStorage.getItem('token')
-                    }
-                }).then( (res) => setProfileData(res.data));
+            await api.get('/stats/user', {
+                headers: {
+                    'Authorization': 'Bearer ' + localStorage.getItem('token')
+                }
+            }).then((res) => setProfileData(res.data));
         };
         fetchData().catch((error) => {
             toast("Dogodila se greška.");
@@ -37,24 +37,20 @@ const MyProfile = () => {
     }, []);
 
     const invalidatePatient = async () => {
-        try {
-            await api.delete('/patient/' + profileData.id, {
-                headers: {
-                    'Authorization': 'Bearer ' + localStorage.getItem('token')
-                }
-            });
-        } catch (error) {
-            toast("Dogodila se greška.");
-        }
+        await api.delete('/patient/' + profileData.id, {
+            headers: {
+                'Authorization': 'Bearer ' + localStorage.getItem('token')
+            }
+        }).catch(() => toast.error("Dogodila se greška."));
     };
 
     const [showOldPass, setShowOldPass] = useState(false);
     const [showPass, setShowPass] = useState(false);
     const [showConfirmPass, setShowConfirmPass] = useState(false);
     const [data, setData] = useState({
-        newPass : '',
-        confirmPass : '',
-        oldPass : ''
+        newPass: '',
+        confirmPass: '',
+        oldPass: ''
     });
     const toggleOldPass = () => {
         setShowOldPass(prevState => !prevState);
@@ -66,8 +62,8 @@ const MyProfile = () => {
         setShowConfirmPass(prevState => !prevState);
     }
     const handleChange = (e) => {
-        const { name, value } = e.target;
-        setData({...data, [name] : value});
+        const {name, value} = e.target;
+        setData({...data, [name]: value});
     }
 
     const validateChangingPasswordData = () => {
@@ -83,44 +79,53 @@ const MyProfile = () => {
             toast.info("Popunite sva polja.")
             return;
         }
+        if (data.oldPass === data.newPass) {
+            toast.warn("Nova lozinka ne smije biti ista kao stara.")
+        }
     }
+
     const handleSubmit = async (e) => {
         e.preventDefault();
-        try {
-            validateChangingPasswordData();
-            await api.post("/auth/change/password", data, {
-                headers: {
-                    'Authorization': 'Bearer ' + localStorage.getItem('token')
-                }
-            }).then(() => {
-                setOpen(false);
-                toast.success("Uspjesno promjenjena lozinka")
-            });
-        } catch (err) {
-            toast("Dogodila se greška.");
-        }
-
+        validateChangingPasswordData();
+        await api.post("/auth/change/password", data, {
+            headers: {
+                'Authorization': 'Bearer ' + localStorage.getItem('token')
+            }
+        }).then(() => {
+            setOpen(false);
+            toast.success("Uspjesno promjenjena lozinka")
+        }).catch(() => toast.error("Dogodila se greška."));
     }
 
     const [open, setOpen] = useState(false);
     const [openInvalidate, setOpenInvalidate] = useState(false);
-    const contentStyle = { borderRadius:'10px', width: '40%'};
+    const contentStyle = {borderRadius: '10px', width: '40%'};
 
 
-
-    return(
+    return (
         <PageLayout>
-            <Header />
-            <span className="text-sky-700 font-bold text-3xl justify-self-center self-center mt-5 mb-5 ">Moj profil</span>
+            <Header/>
+            <span
+                className="text-sky-700 font-bold text-3xl justify-self-center self-center mt-5 mb-5 ">Moj profil</span>
 
-            <PopupJS open={openInvalidate} closeOnDocumentClick={false} onClose={() => setOpenInvalidate(false)} modal {...{contentStyle}}>
+            <PopupJS open={openInvalidate} closeOnDocumentClick={false} onClose={() => setOpenInvalidate(false)}
+                     modal {...{contentStyle}}>
                 <div className="flex justify-center my-10">
                     <div className="flex-1 items-center rounded-bl-[10px] rounded-br-[10px] flex flex-col">
-                        <label className="font-bold text-sky-600 text-2xl mt-[30px] text-center">Želite li deaktivirati račun?</label>
+                        <label className="font-bold text-sky-600 text-2xl mt-[30px] text-center">Želite li deaktivirati
+                            račun?</label>
 
                         <div className="w-full flex justify-around mt-[45px]">
-                            <button className="bg-red-600 text-white px-9 py-2 font-semibold rounded-[5px]" onClick={() => { setOpenInvalidate(false); }}>Ne</button>
-                            <button className="bg-green-800 text-white px-9 py-2 font-semibold rounded-[5px]" onClick={() => { invalidatePatient().then(r => setOpenInvalidate(false)); }}>Da</button>
+                            <button className="bg-red-600 text-white px-9 py-2 font-semibold rounded-[5px]"
+                                    onClick={() => {
+                                        setOpenInvalidate(false);
+                                    }}>Ne
+                            </button>
+                            <button className="bg-green-800 text-white px-9 py-2 font-semibold rounded-[5px]"
+                                    onClick={() => {
+                                        invalidatePatient().then(r => setOpenInvalidate(false));
+                                    }}>Da
+                            </button>
                         </div>
                     </div>
                 </div>
@@ -167,61 +172,70 @@ const MyProfile = () => {
                         </button>
 
                     </form>
-                    <button className="bg-sky-600 text-white p-1 rounded-[5px] absolute top-0 right-0 w-[32px] font-bold font-extrabold"
-                            onClick={() => setOpen(false)}>X
+                    <button
+                        className="bg-sky-600 text-white p-1 rounded-[5px] absolute top-0 right-0 w-[32px] font-bold font-extrabold"
+                        onClick={() => setOpen(false)}>X
                     </button>
                 </div>
             </PopupJS>
             <div>
                 {profileData ? (
-                <table className="flex justify-center items-center">
+                    <table className="flex justify-center items-center">
 
-                    <tbody className={'bg-sky-200 p-8 rounded-[10px]'}>
-                    {!profileData.roles.includes("SUPERADMIN")  && (
-                        <>
-                            <tr>
-                                <td className={'font-bold text-sky-600 text-end text-2xl  pr-10'}>Ime:</td>
-                                <td className={'text-sky-700  text-start text-2xl p-5 pl-10'} >{profileData.firstName}</td>
-                            </tr>
-                            <tr>
-                                <td className={'font-bold text-sky-600 text-end text-2xl  p-5 pr-10'}>Prezime:</td>
-                                <td className={'text-sky-700  text-start text-2xl p-5 pl-10'}>{profileData.lastName}</td>
-                            </tr>
-                        </>
-                    )}
+                        <tbody className={'bg-sky-200 p-8 rounded-[10px]'}>
+                        {!profileData.roles.includes("SUPERADMIN") && (
+                            <>
+                                <tr>
+                                    <td className={'font-bold text-sky-600 text-end text-2xl  pr-10'}>Ime:</td>
+                                    <td className={'text-sky-700  text-start text-2xl p-5 pl-10'}>{profileData.firstName}</td>
+                                </tr>
+                                <tr>
+                                    <td className={'font-bold text-sky-600 text-end text-2xl  p-5 pr-10'}>Prezime:</td>
+                                    <td className={'text-sky-700  text-start text-2xl p-5 pl-10'}>{profileData.lastName}</td>
+                                </tr>
+                            </>
+                        )}
                         <tr>
-                            <td  className={'font-bold text-sky-600 text-end text-2xl p-5 pr-10'}>Email:</td>
+                            <td className={'font-bold text-sky-600 text-end text-2xl p-5 pr-10'}>Email:</td>
                             <td className={'text-sky-700 text-start text-2xl p-5 pl-10'}>{profileData.username}</td>
                         </tr>
                         <tr>
                             <td className={'font-bold text-sky-600 text-end text-2xl p-5 pr-10'}>Uloga:</td>
-                            <td  className={'text-sky-700 text-start text-2xl p-5 pl-10'}>
+                            <td className={'text-sky-700 text-start text-2xl p-5 pl-10'}>
                                 {profileData.roles.includes('ADMIN') ? 'ADMINISTRATOR' :
                                     (profileData.roles.includes('PATIENT') ? 'PACIJENT' :
-                                    (profileData.roles.includes('SUPERADMIN') ? 'SUPER ADMINISTRATOR' :
-                                    (profileData.roles.includes('EMPLOYEE') && profileData.roles.length === 1 ? 'ZAPOSLENIK' :
-                                    'Nije poznata uloga.')))}
-                             </td>
-                            </tr>
+                                        (profileData.roles.includes('SUPERADMIN') ? 'SUPER ADMINISTRATOR' :
+                                            (profileData.roles.includes('EMPLOYEE') && profileData.roles.length === 1 ? 'ZAPOSLENIK' :
+                                                'Nije poznata uloga.')))}
+                            </td>
+                        </tr>
                         <tr>
-                            {profileData.roles.includes("PATIENT")  && (
+                            {profileData.roles.includes("PATIENT") && (
                                 <>
                                     <td className={' text-sky-600 text-center text-2xl p-5 pr-5'}>
-                                            <button className={'bg-sky-600 text-white p-2 px-4 border-none cursor-pointer text-base font-semibold rounded-md'} onClick={() => {setOpenInvalidate(true);}}>
-                                                Deaktiviraj račun
-                                            </button>
+                                        <button
+                                            className={'bg-sky-600 text-white p-2 px-4 border-none cursor-pointer text-base font-semibold rounded-md'}
+                                            onClick={() => {
+                                                setOpenInvalidate(true);
+                                            }}>
+                                            Deaktiviraj račun
+                                        </button>
                                     </td>
                                 </>
                             )}
                             <td className={'text-sky-600 text-center text-2xl p-5 pl-5'}>
-                                <button className={'bg-sky-600 text-white p-2 px-4 border-none cursor-pointer text-base font-semibold rounded-md'} onClick={() => {setOpen(true);}}>
+                                <button
+                                    className={'bg-sky-600 text-white p-2 px-4 border-none cursor-pointer text-base font-semibold rounded-md'}
+                                    onClick={() => {
+                                        setOpen(true);
+                                    }}>
                                     Promijeni lozinku
                                 </button>
                             </td>
                         </tr>
-                    </tbody>
-                </table>
-                ): (
+                        </tbody>
+                    </table>
+                ) : (
                     <p>Učitavanje...</p>
                 )};
             </div>
@@ -238,7 +252,7 @@ const MyProfile = () => {
                 pauseOnHover
                 theme="light"
             />
-            <Footer />
+            <Footer/>
         </PageLayout>
     );
 }
