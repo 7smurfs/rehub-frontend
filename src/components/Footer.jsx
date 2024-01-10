76e8bea28094e3ab76cdb3042740550de7d58c8a
begJosip <josipbegic57@gmail.com>
2024-01-10 18:44:13 +0100

    Add dark mode switch

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,28 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import "../index.css";
+import lightSwitch from "../assets/light_switch.svg";
+import darkSwitch from "../assets/night_switch.svg";
 
 function Footer() {
+    const [isDarkMode, setIsDarkMode] = useState(false);
+
+    useEffect(() => {
+        const storedTheme = sessionStorage.getItem('theme');
+        setIsDarkMode(storedTheme === 'dark');
+    }, []);
+
+    const toggleDarkMode = () => {
+        const newTheme = isDarkMode ? 'light' : 'dark';
+        setIsDarkMode(!isDarkMode);
+        sessionStorage.setItem('theme', newTheme);
+        document.documentElement.classList.toggle('dark', isDarkMode);
+    };
     const currentYear = new Date().getFullYear();
     return (
         <footer className={'cursor-default'}>
             <div className="bg-lightSky text-sm text-darkestSky font-semibold w-full flex justify-center items-center h-12">
+                <img onClick={toggleDarkMode} className={'cursor-pointer absolute h-7 ml-1 left-0'} src={isDarkMode ? lightSwitch : darkSwitch}
+                     alt={'theme_switch'}/>
                 <span className={'z-20'}>&copy; Copyright {currentYear}, ReHub</span>
                 <span className={'min-[330px]:text-heavierGray absolute font-thin right-0 mr-1 text-sm text-lightSky'}>
                     {process.env.REACT_APP_NAME}&nbsp;{process.env.REACT_APP_VERSION}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,44 +1,45 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    colors: {
-      white: '#FFFFFF',
-      black: '#111111',
+    content: [
+        "./src/**/*.{js,jsx,ts,tsx}",
+    ],
+    darkMode: 'class',
+    theme: {
+        colors: {
+            white: '#FFFFFF',
+            black: '#111111',
 
-      transparentSky: '#f0f9ff', // sky-50
-      lighterSky: '#e0f2fe', // sky-100
-      lightSky: '#bae6fd', // sky-200
-      mediumSky: '#0284c7', // sky-600
-      darkSky: '#075985', // sky-800
-      darkerSky: '#0c4a6e', // sky-900
-      darkestSky: '#082f49', // sky-950
+            transparentSky: '#f0f9ff', // sky-50
+            lighterSky: '#e0f2fe', // sky-100
+            lightSky: '#bae6fd', // sky-200
+            mediumSky: '#0284c7', // sky-600
+            darkSky: '#075985', // sky-800
+            darkerSky: '#0c4a6e', // sky-900
+            darkestSky: '#082f49', // sky-950
 
-      lightGray: '#d1d5db', // gray-300
-      mediumGray: '#9ca3af', // gray-400
-      gray: '#6b7280', // gray-500
-      heavyGray: '#4b5563', // gray-600
-      heavierGray: '#3f3f46', // gray-700
+            lightGray: '#d1d5db', // gray-300
+            mediumGray: '#9ca3af', // gray-400
+            gray: '#6b7280', // gray-500
+            heavyGray: '#4b5563', // gray-600
+            heavierGray: '#3f3f46', // gray-700
 
-      redLight: '#f87171', // red-400
-      redMedium: '#dc2626', // red-600
-      redHeavy: '#450a0a', // red-950
+            redLight: '#f87171', // red-400
+            redMedium: '#dc2626', // red-600
+            redHeavy: '#450a0a', // red-950
 
-      green: '#16a34a',
-      greenMedium: '#047857',
-      greenHeavy: '#064e3b',
+            green: '#16a34a',
+            greenMedium: '#047857',
+            greenHeavy: '#064e3b',
 
-      heavyYellow: '#854d0e',
-      lightYellow: '#facc15'
+            heavyYellow: '#854d0e',
+            lightYellow: '#facc15'
+        },
+        extend: {
+            backgroundImage: {
+                'landing-bg': "url('assets/landing-page-bg.webp')"
+            }
+        },
     },
-    extend: {
-      backgroundImage: {
-        'landing-bg': "url('assets/landing-page-bg.webp')"
-      }
-    },
-  },
-  plugins: [],
+    plugins: [],
 }
 
