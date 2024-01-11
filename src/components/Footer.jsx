2deda695ce12590922d4acae8896d546638c3346
begJosip <josipbegic57@gmail.com>
2024-01-11 01:47:51 +0100

    FIX: fix dark mode switch

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,19 +9,21 @@ function Footer() {
     useEffect(() => {
         const storedTheme = sessionStorage.getItem('theme');
         setIsDarkMode(storedTheme === 'dark');
+        document.documentElement.classList.toggle('dark', storedTheme === 'dark');
     }, []);
 
     const toggleDarkMode = () => {
         const newTheme = isDarkMode ? 'light' : 'dark';
         setIsDarkMode(!isDarkMode);
         sessionStorage.setItem('theme', newTheme);
-        document.documentElement.classList.toggle('dark', isDarkMode);
+        document.documentElement.classList.toggle('dark', !isDarkMode);
     };
+
     const currentYear = new Date().getFullYear();
     return (
         <footer className={'cursor-default'}>
             <div className="bg-lightSky dark:bg-black text-sm text-darkestSky dark:text-white font-semibold w-full flex justify-center items-center h-12">
-                <img onClick={toggleDarkMode} className={'cursor-pointer absolute h-7 ml-1 left-0'} src={isDarkMode ? lightSwitch : darkSwitch}
+                <img onClick={toggleDarkMode} className={'cursor-pointer absolute h-7 ml-1 left-0'} src={isDarkMode ? darkSwitch : lightSwitch}
                      alt={'theme_switch'}/>
                 <span className={'z-20'}>&copy; Copyright {currentYear}, ReHub</span>
                 <span className={'min-[330px]:text-heavierGray absolute font-thin right-0 mr-1 text-sm text-lightSky dark:text-mediumGray'}>
